09313fecdfd397879cf8308a8afcb1d9dc638999
Vladimir Vivien <vvivien@xor.exchange>
2015-09-16 17:25:27 -0400

    Adding logging support

--- proc/logrus.go
+++ proc/logrus.go
@@ -1,4 +1,63 @@
 package proc
 
+import (
+	"fmt"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/vladimirvivien/automi/api"
+)
+
+// LogrusProc is a sink component that uses Logrus to log
+// * logrus.Entry
+// * api.ProcError
 type LogrusProc struct {
+	Name   string
+	Logger *logrus.Logger
+	Input  <-chan interface{}
+}
+
+func (p *LogrusProc) Init() error {
+	if p.Name == "" {
+		return api.ProcError{Err: fmt.Errorf("Missing name attribute")}
+	}
+
+	if p.Logger == nil {
+		return api.ProcError{
+			ProcName: p.Name,
+			Err:      fmt.Errorf("Missing Logger attribute"),
+		}
+	}
+
+	if p.Input == nil {
+		return api.ProcError{
+			ProcName: p.Name,
+			Err:      fmt.Errorf("Missing Input attribute"),
+		}
+	}
+
+	return nil
+}
+
+func (p *LogrusProc) Uninit() error {
+	return nil
+}
+
+func (p *LogrusProc) GetName() string {
+	return p.Name
+}
+
+func (p *LogrusProc) GetInput() <-chan interface{} {
+	return p.Input
+}
+
+func (p *LogrusProc) Exec() error {
+	go func() {
+		for item := range p.Input {
+			switch log := item.(type) {
+			case api.ProcError, error:
+				p.Logger.Errorln(log)
+			}
+		}
+	}()
+	return nil
 }
