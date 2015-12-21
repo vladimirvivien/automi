6567fbdc631c45b433a1963f7b459dd9380097e9
Vladimir Vivien <vladimir.vivien@gmail.com>
2015-12-21 07:35:37 -0500

    Added csv source, test, benchmark

--- file/csvsource.go
+++ file/csvsource.go
@@ -12,25 +12,37 @@ import (
 	autoctx "github.com/vladimirvivien/automi/context"
 )
 
-// CsvSource implements an Source process that reads the content of a
+// CsvSrc implements an Source process that reads the content of a
 // specified file and emits its record via its Output Channel
 // and serializes each row as a slice []string.
-type CsvSource struct {
+type CsvSrc struct {
 	filepath    string   // path for the file
 	delimChar   rune     // Delimiter charater, defaults to comma
-	commentChar rune     // Charater indicating line is a comment
+	commentChar rune     // Charater indicating line is a cg.org/omment
 	headers     []string // Column header names (specified here or read from file)
 	hasHeaders  bool     // indicates first row is for headers (default false).
 	fieldCount  int      // if greater than zero is used to validate field count
 
-	file   *os.File
-	reader *csv.Reader
-	log    *logrus.Entry
-	output chan interface{}
+	file      *os.File
+	srcReader io.Reader
+	csvReader *csv.Reader
+	log       *logrus.Entry
+	output    chan interface{}
 }
 
-func NewCsvSource(path string) *CsvSource {
-	csv := &CsvSource{
+func CsvSource(reader io.Reader) *CsvSrc {
+	csv := &CsvSrc{
+		srcReader:   reader,
+		delimChar:   ',',
+		commentChar: '#',
+		output:      make(chan interface{}, 1024),
+	}
+
+	return csv
+}
+
+func CsvSourceFile(path string) *CsvSrc {
+	csv := &CsvSrc{
 		filepath:    path,
 		delimChar:   ',',
 		commentChar: '#',
@@ -40,36 +52,32 @@ func NewCsvSource(path string) *CsvSource {
 	return csv
 }
 
-func (c *CsvSource) DelimChar(char rune) *CsvSource {
+func (c *CsvSrc) DelimChar(char rune) *CsvSrc {
 	c.delimChar = char
 	return c
 }
 
-func (c *CsvSource) CommentChar(char rune) *CsvSource {
+func (c *CsvSrc) CommentChar(char rune) *CsvSrc {
 	c.commentChar = char
 	return c
 }
 
-func (c *CsvSource) HasHeaders() *CsvSource {
+func (c *CsvSrc) HasHeaders() *CsvSrc {
 	c.hasHeaders = true
 	return c
 }
 
-func (c *CsvSource) init(ctx context.Context) error {
+func (c *CsvSrc) init(ctx context.Context) error {
 	// extract logger
 	log, ok := autoctx.GetLogEntry(ctx)
 	if !ok {
-		log = logrus.WithField("Component", "CsvSource")
+		log = logrus.WithField("Component", "CsvSrc")
 		log.Error("No logger found incontext")
 	}
 	c.log = log.WithFields(logrus.Fields{
-		"Component": "CsvSource",
+		"Component": "CsvSrc",
 		"Type":      fmt.Sprintf("%T", c),
 	})
-
-	if c.filepath == "" {
-		return fmt.Errorf("Missing required filepath attribute")
-	}
 
 	// establish defaults
 	if c.delimChar == 0 {
@@ -80,20 +88,29 @@ func (c *CsvSource) init(ctx context.Context) error {
 		c.commentChar = '#'
 	}
 
-	// open file
-	file, err := os.Open(c.filepath)
-	if err != nil {
-		return fmt.Errorf("Failed to open file: %s ", err)
+	var reader io.Reader
+
+	if c.srcReader != nil {
+		reader = c.srcReader
+		c.log.Info("Using IO Reader")
+	} else {
+		// open file
+		file, err := os.Open(c.filepath)
+		if err != nil {
+			return fmt.Errorf("Failed to open file: %s ", err)
+		}
+		reader = file
+		c.file = file
+		c.log.Info("Using source file ", file.Name())
 	}
 
-	c.file = file
-	c.reader = csv.NewReader(file)
-	c.reader.Comment = c.commentChar
-	c.reader.Comma = c.delimChar
+	c.csvReader = csv.NewReader(reader)
+	c.csvReader.Comment = c.commentChar
+	c.csvReader.Comma = c.delimChar
 
 	// resolve header and field count
 	if c.hasHeaders {
-		if headers, err := c.reader.Read(); err == nil {
+		if headers, err := c.csvReader.Read(); err == nil {
 			c.fieldCount = len(headers)
 			c.headers = headers
 		} else {
@@ -105,16 +122,16 @@ func (c *CsvSource) init(ctx context.Context) error {
 		}
 	}
 	log.Debug("hasHeaders ", c.hasHeaders, " headers [", c.headers, "]")
-	c.log.Info("CsvSource init'd OK: reading from file ", c.file.Name())
+	c.log.Info("Source initializede")
 
 	return nil
 }
 
-func (c *CsvSource) GetOutput() <-chan interface{} {
+func (c *CsvSrc) GetOutput() <-chan interface{} {
 	return c.output
 }
 
-func (c *CsvSource) Open(ctx context.Context) (err error) {
+func (c *CsvSrc) Open(ctx context.Context) (err error) {
 	if err = c.init(ctx); err != nil {
 		return
 	}
@@ -124,16 +141,17 @@ func (c *CsvSource) Open(ctx context.Context) (err error) {
 	go func() {
 		defer func() {
 			close(c.output)
-			err = c.file.Close()
-			if err != nil {
-				c.log.Error(err)
+			if c.file != nil {
+				err = c.file.Close()
+				if err != nil {
+					c.log.Error(err)
+				}
 			}
 			c.log.Info("Source closed")
 		}()
 
 		for {
-			row, err := c.reader.Read()
-
+			row, err := c.csvReader.Read()
 			if err != nil {
 				if err == io.EOF {
 					return
