a4a963303294d0580c3cb328bf6d7a8962984c5b
Vladimir Vivien <vladimir.vivien@gmail.com>
2016-02-16 09:45:42 -0500

    added sink Open method, see #14.

--- sinks/db/dbsink.go
+++ sinks/db/dbsink.go
@@ -12,9 +12,11 @@ import (
 )
 
 type DbSink struct {
-	input <-chan interface{}
-	db    api.Database
-	log   *logrus.Entry
+	input  <-chan interface{}
+	db     api.Database
+	log    *logrus.Entry
+	sql    string
+	prepFn func(interface{}) []interface{}
 }
 
 func New() *DbSink {
@@ -23,6 +25,16 @@ func New() *DbSink {
 
 func (s *DbSink) WithDB(db api.Database) *DbSink {
 	s.db = db
+	return s
+}
+
+func (s *DbSink) Prepare(fn func(interface{}) []interface{}) *DbSink {
+	s.prepFn = fn
+	return s
+}
+
+func (s *DbSink) Sql(sql string) *DbSink {
+	s.sql = sql
 	return s
 }
 
@@ -46,6 +58,14 @@ func (s *DbSink) init(ctx context.Context) error {
 		return fmt.Errorf("Input attribute not set")
 	}
 
+	if s.sql == "" {
+		return fmt.Errorf("Sql attribute is required")
+	}
+
+	if s.prepFn == nil {
+		return fmt.Errorf("Prepare function is missing")
+	}
+
 	s.log.Info("Component initialized")
 
 	return nil
@@ -60,5 +80,52 @@ func (s *DbSink) Open(ctx context.Context) <-chan error {
 		}()
 		return result
 	}
+
+	go func() {
+		defer func() {
+			close(result)
+			s.db.Close()
+			s.log.Info("Component closed")
+		}()
+		stmt, err := s.db.Prepare(s.sql)
+		if err != nil {
+			result <- err
+			return
+		}
+
+		for item := range s.input {
+			args := s.prepFn(item) // prepare sql args
+
+			tx, err := s.db.Begin()
+			if err != nil {
+				result <- err
+				return
+			}
+
+			// exec sql within tx
+			_, err = tx.Stmt(stmt).Exec(args)
+			if err != nil {
+				s.log.Error(err) // log sql error, continue
+				if rberr := tx.Rollback(); rberr != nil {
+					// something maybe wrong, stop
+					s.log.Error(rberr)
+					result <- rberr
+					return
+				}
+				continue
+			}
+
+			// commit tx
+			if err := tx.Commit(); err != nil {
+				s.log.Error(err)
+				if rberr := tx.Rollback(); rberr != nil {
+					s.log.Errorf("Rollback failed %v", err)
+					result <- err
+					return
+				}
+				continue
+			}
+		}
+	}()
 	return result
 }
