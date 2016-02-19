101a009a111e07ad30e192e45005ac345fe13b58
Vladimir Vivien <vladimir.vivien@gmail.com>
2016-02-19 14:34:36 -0500

    New GroupByInt test for different scenarios

--- stream/stream_group_test.go
+++ stream/stream_group_test.go
@@ -8,7 +8,8 @@ import (
 	"github.com/vladimirvivien/automi/api/tuple"
 )
 
-func TestStream_GroupByInt(t *testing.T) {
+// TestStream_GroupByInt_KV groups incoming tuple.KV data items.
+func TestStream_GroupByInt_KV(t *testing.T) {
 	src := NewSliceSource("Hello World", "Hello Milkyway", "Hello Universe")
 	snk := NewDrain()
 	strm := New().From(src).FlatMap(func(line string) []string {
@@ -23,6 +24,42 @@ func TestStream_GroupByInt(t *testing.T) {
 		result := <-snk.GetOutput()
 		m := result.(map[interface{}][]interface{})
 		if len(m["Hello"]) != 3 {
+			t.Fatal("Items not grouped properly")
+		}
+
+	}()
+
+	select {
+	case err := <-strm.Open():
+		if err != nil {
+			t.Fatal(err)
+		}
+		select {
+		case <-wait:
+		case <-time.After(10 * time.Millisecond):
+			t.Fatal("Stream took too long to complete")
+		}
+	case <-time.After(10 * time.Millisecond):
+		t.Fatal("Took too long")
+	}
+
+}
+
+// TestStream_GroupByInt_Slice tests  grouping data items received as slice.
+func TestStream_GroupByInt_Slice(t *testing.T) {
+	src := NewSliceSource("Hello World Wide", "Hello Milkyway Away", "Our World Now")
+	snk := NewDrain()
+	strm := New().From(src).Map(func(item string) []string {
+		return strings.Split(item, " ")
+	}).GroupBy(1).To(snk)
+
+	wait := make(chan struct{})
+	go func() {
+		defer close(wait)
+		result := <-snk.GetOutput()
+		m := result.(map[interface{}][]interface{})
+		t.Log(m)
+		if len(m["World"]) != 4 {
 			t.Fatal("Items not grouped properly")
 		}
 
