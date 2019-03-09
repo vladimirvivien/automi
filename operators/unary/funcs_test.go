47a9b98c3b97502f549a464a0a6bf799e4186f8d
Vladimir Vivien <vladimir.vivien@gmail.com>
2019-03-09 09:59:33 -0500

    Add tests for unary functions with context passing support

--- operators/unary/funcs_test.go
+++ operators/unary/funcs_test.go
@@ -1,25 +1,53 @@
 package unary
 
 import (
-	"bytes"
 	"context"
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/vladimirvivien/automi/api"
 )
 
+type unaryFuncTestCase struct {
+	name           string
+	opBuilder      func(interface{}) (api.UnFunc, error)
+	procFunc       interface{}
+	input          interface{}
+	expected       interface{}
+	ctx            context.Context
+	funcShouldFail bool
+	opShouldFail   bool
+}
+
+func testUnaryFunc(t *testing.T, test unaryFuncTestCase) {
+	op, err := test.opBuilder(test.procFunc)
+
+	switch {
+	case test.funcShouldFail && err == nil:
+		t.Errorf("expecting failure, but error is nil")
+	case test.funcShouldFail && err != nil:
+		t.Log(err)
+	case !test.funcShouldFail:
+		if err != nil {
+			t.Fatal(err)
+		}
+		result := op.Apply(test.ctx, test.input)
+		if !test.opShouldFail && !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("expecting %v got %v", test.expected, result)
+		}
+	case test.opShouldFail:
+		if err == nil {
+			t.Fatal("expecting failure, but error is nil")
+		}
+	}
+}
+
 func TestUnaryFunc_Process(t *testing.T) {
-	tests := []struct {
-		name           string
-		procFunc       interface{}
-		input          interface{}
-		expected       interface{}
-		ctx            context.Context
-		funcShouldFail bool
-		opShouldFail   bool
-	}{
+	tests := []unaryFuncTestCase{
 		{
-			name: "unary funcForm1, OK",
+			name:      "unary funcForm1, OK",
+			opBuilder: ProcessFunc,
 			procFunc: func(item int) int {
 				return item * 2
 			},
@@ -27,7 +55,8 @@ func TestUnaryFunc_Process(t *testing.T) {
 			expected: 12,
 		},
 		{
-			name: "unary funcForm1, procFunc fail",
+			name:      "unary funcForm1, procFunc fail",
+			opBuilder: ProcessFunc,
 			procFunc: func(item string) string {
 				return strings.ToUpper(item)
 			},
@@ -36,8 +65,9 @@ func TestUnaryFunc_Process(t *testing.T) {
 			opShouldFail: true,
 		},
 		{
-			name: "unary funcForm2, OK",
-			ctx:  context.Background(),
+			name:      "unary funcForm2, OK",
+			opBuilder: ProcessFunc,
+			ctx:       context.Background(),
 			procFunc: func(ctx context.Context, item string) string {
 				return strings.ToUpper(item)
 			},
@@ -45,7 +75,8 @@ func TestUnaryFunc_Process(t *testing.T) {
 			expected: "HELLO",
 		},
 		{
-			name: "unary with two returns",
+			name:      "unary with two returns",
+			opBuilder: ProcessFunc,
 			procFunc: func(item string) (string, int) {
 				return item, 0
 			},
@@ -55,84 +86,192 @@ func TestUnaryFunc_Process(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			op, err := ProcessFunc(test.procFunc)
-			if test.funcShouldFail && err == nil {
-				t.Errorf("expecting failure, but error is nil")
-			}
-			switch {
-			case !test.funcShouldFail:
-				if err != nil {
-					t.Fatal(err)
-				}
-				result := op.Apply(test.ctx, test.input)
-				if !test.opShouldFail && !reflect.DeepEqual(result, test.expected) {
-					t.Errorf("expecting %v got %v", test.expected, result)
-				}
-			case test.opShouldFail:
-				if err == nil {
-					t.Fatal("expecting failure, but error is nil")
-				}
-			}
+			testUnaryFunc(t, test)
 		})
 	}
-
 }
-
 func TestUnaryFunc_Filter(t *testing.T) {
-	op, err := FilterFunc(func(item string) bool {
-		return item[0] == []byte("T")[0]
-	})
+	tests := []unaryFuncTestCase{
+		{
+			name:      "filter out data with form f(in)bool",
+			opBuilder: FilterFunc,
+			input:     []string{"Mon", "Tue", "Wed"},
+			procFunc: func(days []string) bool {
+				return (len(days) < 3)
+			},
+			expected: nil,
+		},
+		{
+			name:      "allow with form f(in)bool",
+			opBuilder: FilterFunc,
+			input:     []string{"Mon", "Tue", "Wed"},
+			procFunc: func(days []string) bool {
+				return (len(days) >= 3)
+			},
+			expected: []string{"Mon", "Tue", "Wed"},
+		},
+		{
+			name:      "unexpected return value with form f(in)bool",
+			opBuilder: FilterFunc,
+			input:     "hello",
+			procFunc: func(item string) bool {
+				return (item == "HELLO")
+			},
+			expected:     "HELLO",
+			opShouldFail: true,
+		},
+		{
+			name:      "filter out data with form f(ctx,in)bool",
+			opBuilder: FilterFunc,
+			ctx:       context.Background(),
+			input:     []string{"Mon", "Tue", "Wed"},
+			procFunc: func(ctx context.Context, days []string) bool {
+				return (len(days) < 3)
+			},
+			expected: nil,
+		},
+		{
+			name:      "filter bad with func f(in)(bool,err)",
+			opBuilder: FilterFunc,
+			input:     []string{"Mon", "Tue", "Wed"},
+			procFunc: func(days []string) (bool, error) {
+				return (len(days) < 3), nil
+			},
+			funcShouldFail: true,
+		},
+		{
+			name:      "filter bad with func f(ctx, in)(bool,err)",
+			opBuilder: FilterFunc,
+			input:     []string{"Mon", "Tue", "Wed"},
+			procFunc: func(ctx context.Context, days []string) (bool, error) {
+				return (len(days) < 3), nil
+			},
+			funcShouldFail: true,
+		},
+	}
 
-	if err != nil {
-		t.Fatal(err)
-	}
-	ctx := context.TODO()
-	var strings []string
-	for _, v := range []string{"Mon", "Wed", "Tr", "Sun", "Tu", "Sat"} {
-		result := op.Apply(ctx, v)
-		str, ok := result.(string)
-		if ok {
-			strings = append(strings, str)
-		}
-	}
-	if len(strings) != 2 {
-		t.Fatal("unexpected result from FilterFunc:", strings)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testUnaryFunc(t, test)
+		})
 	}
 }
 
 func TestUnaryFunc_Map(t *testing.T) {
-	op, err := MapFunc(func(item rune) rune {
-		return item - 32
-	})
-
-	if err != nil {
-		t.Fatal(err)
+	tests := []unaryFuncTestCase{
+		{
+			name:      "Map data with function f(in)out",
+			opBuilder: MapFunc,
+			input:     []byte{'H', 'e', 'l', 'l', 'o'},
+			procFunc: func(item []byte) string {
+				return string(item)
+			},
+			expected: "Hello",
+		},
+		{
+			name:      "Map data with op f(in)out with failure",
+			opBuilder: MapFunc,
+			input:     'A',
+			procFunc: func(item rune) rune {
+				return item - 32
+			},
+			expected:     'A' - 31,
+			opShouldFail: true,
+		},
+		{
+			name:      "Map data with function f(ctx, in)out",
+			opBuilder: MapFunc,
+			ctx:       context.Background(),
+			input:     []byte{'H', 'e', 'l', 'l', 'o'},
+			procFunc: func(ctx context.Context, item []byte) string {
+				return string(item)
+			},
+			expected: "Hello",
+		},
+		{
+			name:      "Map data with function f(ctx,in)out with op failure",
+			opBuilder: MapFunc,
+			input:     'H',
+			procFunc: func(item rune) string {
+				return string(item)
+			},
+			expected:     "H",
+			opShouldFail: true,
+		},
+		{
+			name:      "Map data with function f(ctx,in)(out0,out1) with op failure",
+			opBuilder: MapFunc,
+			input:     'H',
+			procFunc: func(item rune) (string, string) {
+				return string(item), string(item)
+			},
+			expected:       "HH",
+			funcShouldFail: true,
+		},
 	}
 
-	str := new(bytes.Buffer)
-	ctx := context.TODO()
-	for _, v := range []rune{'h', 'e', 'l', 'l', 'o'} {
-		result := op.Apply(ctx, v)
-		chr := result.(rune)
-		str.WriteRune(chr)
-	}
-
-	if str.String() != "HELLO" {
-		t.Fatal("unexpected result from MapFunc:", str)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testUnaryFunc(t, test)
+		})
 	}
 }
 
 func TestUnaryFunc_FlatMap(t *testing.T) {
-	op, err := FlatMapFunc(func(msg string) []rune {
-		return []rune(msg)
-	})
-	if err != nil {
-		t.Fatal(err)
+	tests := []unaryFuncTestCase{
+		{
+			name:      "FlatMap data func function f(in)out",
+			opBuilder: FlatMapFunc,
+			input:     "Hello",
+			procFunc: func(item string) []rune {
+				return []rune(item)
+			},
+			expected: []rune("Hello"),
+		},
+		{
+			name:      "FlatMap data with func f(in)out with wrong result param type",
+			opBuilder: FlatMapFunc,
+			input:     "Hello",
+			procFunc: func(item string) string {
+				return item
+			},
+			funcShouldFail: true,
+		},
+		{
+			name:      "FlatMap data op fun f(ctx,in)out",
+			ctx:       context.Background(),
+			opBuilder: FlatMapFunc,
+			input:     "Hello",
+			procFunc: func(item string) []rune {
+				return []rune(item)
+			},
+			expected: []rune("Hello"),
+		},
+		{
+			name:      "FlatMap data op func f(ctx,in)out",
+			ctx:       context.Background(),
+			opBuilder: FlatMapFunc,
+			input:     "Hello",
+			procFunc: func(item string) []rune {
+				return []rune(item)
+			},
+			expected:     []rune("Bello"),
+			opShouldFail: true,
+		},
+		{
+			name:      "FlatMap data with func f(ctx,in)(out0,out1) with op failure",
+			opBuilder: MapFunc,
+			input:     `Hello`,
+			procFunc: func(item string) ([]byte, []byte) {
+				return []byte(item), []byte(item)
+			},
+			funcShouldFail: true,
+		},
 	}
-	ctx := context.TODO()
-	result := op.Apply(ctx, "hello")
-	slice := result.([]rune)
-	if len(slice) != 5 {
-		t.Fatal("unexpected result from FlatMapFunc:", len(slice))
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testUnaryFunc(t, test)
+		})
 	}
 }
